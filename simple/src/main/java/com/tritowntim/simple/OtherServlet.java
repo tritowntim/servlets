3c2d8c3d851c121878413c404ab46970186c0a03
Tim Dussinger <tritowntim@gmail.com>
2012-08-11 16:22:41 -0400

    Servlet init() method, object ID and thread ID logging

--- simple/src/main/java/com/tritowntim/simple/OtherServlet.java
+++ simple/src/main/java/com/tritowntim/simple/OtherServlet.java
@@ -11,9 +11,16 @@ import javax.servlet.http.HttpServletResponse;
 public class OtherServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
-
+	private Date initializedAt;
+	private int objectID;
+	
 	private String p(String text) {
 		return "<p>" + text + "</p>";
+	}
+	
+	public void init() {
+		initializedAt = new Date();
+		objectID = this.hashCode();
 	}
 	
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -21,14 +28,18 @@ public class OtherServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		Date today = new Date();
 		String title = "Other Servlet";
+		int thread = Thread.currentThread().hashCode();
+		
 		
 		StringBuffer res = new StringBuffer();
 		res.append("<html>");
 		res.append("<head><title>" +  title + "</title></head>");
 		res.append("<body>");
 		res.append("<h4>" + title + "</h4>");
-		res.append(p(today.toLocaleString()));
 		res.append(p(request.getRequestURI()));
+		res.append(p("thread " + thread));
+		res.append(p("original object " + objectID + " initialized at " + initializedAt.toLocaleString()));
+		res.append(p("this object " + this.hashCode() + " accessed at " + today.toLocaleString()));
 		res.append("</body>");
 		res.append("</html>");
 		out.println(res.toString());
